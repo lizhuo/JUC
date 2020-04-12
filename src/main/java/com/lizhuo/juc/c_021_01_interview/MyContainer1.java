4ce465502e49e0fada02c6a275e8b747f74453d0
lizhuo <lizhuo@yy.com>
2020-04-12 18:12:37 +0800

    file encoding

--- src/main/java/com/lizhuo/juc/c_021_01_interview/MyContainer1.java
+++ src/main/java/com/lizhuo/juc/c_021_01_interview/MyContainer1.java
@@ -1,9 +1,9 @@
 /**
- * �����⣺дһ���̶�����ͬ��������ӵ��put��get�������Լ�getCount������
- * �ܹ�֧��2���������߳��Լ�10���������̵߳���������
- * 
- * ʹ��wait��notify/notifyAll��ʵ��
- * 
+ * 面试题：写一个固定容量同步容器，拥有put和get方法，以及getCount方法，
+ * 能够支持2个生产者线程以及10个消费者线程的阻塞调用
+ *
+ * 使用wait和notify/notifyAll来实现
+ *
  * @author lizhuo
  */
 package com.lizhuo.juc.c_021_01_interview;
@@ -13,12 +13,12 @@ import java.util.concurrent.TimeUnit;
 
 public class MyContainer1<T> {
 	final private LinkedList<T> lists = new LinkedList<>();
-	final private int MAX = 10; //���10��Ԫ��
+	final private int MAX = 10; //最多10个元素
 	private int count = 0;
 	
 	
 	public synchronized void put(T t) {
-		while(lists.size() == MAX) { //����Ϊʲô��while��������if��
+		while(lists.size() == MAX) { //想想为什么用while而不是用if？
 			try {
 				this.wait(); //effective java
 			} catch (InterruptedException e) {
@@ -28,7 +28,7 @@ public class MyContainer1<T> {
 		
 		lists.add(t);
 		++count;
-		this.notifyAll(); //֪ͨ�������߳̽�������
+		this.notifyAll(); //通知消费者线程进行消费
 	}
 	
 	public synchronized T get() {
@@ -42,13 +42,13 @@ public class MyContainer1<T> {
 		}
 		t = lists.removeFirst();
 		count --;
-		this.notifyAll(); //֪ͨ�����߽�������
+		this.notifyAll(); //通知生产者进行生产
 		return t;
 	}
 	
 	public static void main(String[] args) {
 		MyContainer1<String> c = new MyContainer1<>();
-		//�����������߳�
+		//启动消费者线程
 		for(int i=0; i<10; i++) {
 			new Thread(()->{
 				for(int j=0; j<5; j++) System.out.println(c.get());
@@ -60,8 +60,8 @@ public class MyContainer1<T> {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		
-		//�����������߳�
+
+		//启动生产者线程
 		for(int i=0; i<2; i++) {
 			new Thread(()->{
 				for(int j=0; j<25; j++) c.put(Thread.currentThread().getName() + " " + j);
