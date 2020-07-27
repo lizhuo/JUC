bdb42b800562f365d92656e8f9b96ead68ed366a
lizhuo <lizhuo@yy.com>
2020-07-27 15:33:09 +0800

    Vector -- Queue

--- src/main/java/com/lizhuo/juc/c_024_FromVectorToQueue/TicketSeller1.java
+++ src/main/java/com/lizhuo/juc/c_024_FromVectorToQueue/TicketSeller1.java
@@ -2,10 +2,11 @@
  * 有N张火车票，每张票都有一个编号
  * 同时有10个窗口对外售票
  * 请写一个模拟程序
- *
+ * <p>
  * 分析下面的程序可能会产生哪些问题？
  * 重复销售？超量销售？
- * 
+ *
+ * 结论：没有加锁 会发生超卖
  */
 package com.lizhuo.juc.c_024_FromVectorToQueue;
 
@@ -14,17 +15,16 @@ import java.util.List;
 
 public class TicketSeller1 {
 	static List<String> tickets = new ArrayList<>();
-	
+
 	static {
-		for(int i=0; i<10000; i++) tickets.add("ticket no:" + i);
+		for (int i = 0; i < 10000; i++) tickets.add("ticket no:" + i);
 	}
-	
-	
-	
+
+
 	public static void main(String[] args) {
-		for(int i=0; i<10; i++) {
-			new Thread(()->{
-				while(tickets.size() > 0) {
+		for (int i = 0; i < 10; i++) {
+			new Thread(() -> {
+				while (tickets.size() > 0) {
 					// 并发无锁 销售超卖 打印 sale null
 					System.out.println("sale " + tickets.remove(0));
 				}
--- src/main/java/com/lizhuo/juc/c_024_FromVectorToQueue/TicketSeller2.java
+++ src/main/java/com/lizhuo/juc/c_024_FromVectorToQueue/TicketSeller2.java
@@ -2,12 +2,13 @@
  * 有N张火车票，每张票都有一个编号
  * 同时有10个窗口对外售票
  * 请写一个模拟程序
- *
+ * <p>
  * 分析下面的程序可能会产生哪些问题？
- *
+ * <p>
  * 使用Vector或者Collections.synchronizedXXX
  * 分析一下，这样能解决问题吗？
  *
+ * 结论：仍旧发生超卖 size 与 remove 不是原子性操作
  */
 package com.lizhuo.juc.c_024_FromVectorToQueue;
 
@@ -16,26 +17,25 @@ import java.util.concurrent.TimeUnit;
 
 public class TicketSeller2 {
 	static Vector<String> tickets = new Vector<>();
-	
-	
+
+
 	static {
-		for(int i=0; i<1000; i++) tickets.add("ticket no:" + i);
+		for (int i = 0; i < 1000; i++) tickets.add("ticket no:" + i);
 	}
-	
+
 	public static void main(String[] args) {
-		
-		for(int i=0; i<10; i++) {
-			new Thread(()->{
+
+		for (int i = 0; i < 10; i++) {
+			new Thread(() -> {
 				// size() remove() 整体并非原子
-				while(tickets.size() > 0) {
-					
+				while (tickets.size() > 0) {
+
 					try {
 						TimeUnit.MILLISECONDS.sleep(10);
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
-					
-					
+
 					System.out.println("sale--" + tickets.remove(0));
 				}
 			}).start();
